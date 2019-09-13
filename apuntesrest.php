6c752fd1633af3ce7bf18d95619ab628963861f6
Juan Diego <juanrojax05@gmail.com>
2019-09-13 18:10:24 -0500

    Commit Clase 2

---
+++ apuntesrest.php
@@ -0,0 +1,7 @@
+CLASE 2
+- Que es una API Aplication Programming Interfaces conjuto de reglas que se establecen con que las aplicaciones se cominucan entre similar_text
+Es decir quien incia la comunicacion 
+que respuestas esperar 
+que mensajes se envian 
+
+CLASE 3
