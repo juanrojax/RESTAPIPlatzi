7a915bec54e50a0282bf0ba939c220c9b4e92cf8
Juan Diego <juanrojax05@gmail.com>
2019-09-13 19:08:23 -0500

    Commit Clase 5

--- apuntesrest.php
+++ apuntesrest.php
@@ -5,3 +5,19 @@ que respuestas esperar
 que mensajes se envian 
 
 CLASE 3
+HTTP: Protocolo de transferencia de hipertextos (Conjunto de reglas de comunicacion) 
+
+CLASE 4 QUE ES REST Y APIRESTFUL
+REST estilo de arquitectura enfocado en el intercambio de recusos. es un protocolo de intercambio de recusos 
+
+APIRESTFUL Api diseñada con los conseptos de REST
+
+conseptos REST
+Recurso
+URI
+Accion
+
+Peticion REST se basa en una URL y un verbo HTTP
+EJEMPLO GET /books/1 DELETE /books/50
+
+CLASE 5
